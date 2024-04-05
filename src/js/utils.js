10b794d76e33b275d44fe3f90368a26a70de22c9
Philip Sosnin <fff26@mail.ru>
2024-04-05 12:20:16 +0400

    Remake function orderByProps

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,27 +1,22 @@
 export function orderByProps(obj, order) {
-    const orderedProps = [];
-    const restProps = [];
-  
-    // Сначала добавляем свойства в порядке из массива order
-    for (const prop of order) {
-      if (prop in obj) {
-        orderedProps.push({ key: prop, value: obj[prop] });
-      }
+  const orderedProps = [];
+  const restProps = [];
+
+  // Добавляем свойства в порядке из массива order и оставшиеся свойства в алфавитном порядке
+  for (const prop in obj) {
+    if (order.includes(prop)) {
+      orderedProps.push({ key: prop, value: obj[prop] });
+    } else {
+      restProps.push({ key: prop, value: obj[prop] });
     }
-  
-    // Добавляем оставшиеся свойства в алфавитном порядке
-    for (const prop in obj) {
-      if (!order.includes(prop)) {
-        restProps.push({ key: prop, value: obj[prop] });
-      }
-    }
-  
-    // Сортируем оставшиеся свойства по ключу
-    restProps.sort((a, b) => (a.key > b.key ? 1 : -1));
-  
-    // Объединяем отсортированные свойства
-    return [...orderedProps, ...restProps];
   }
+
+  // Сортируем оставшиеся свойства по ключу
+  restProps.sort((a, b) => (a.key > b.key ? 1 : -1));
+
+  // Объединяем отсортированные свойства
+  return [...orderedProps, ...restProps];
+}
   
   export function extractSpecials({ special }) {
     return special.map(({ id, name, icon, description = 'Описание недоступно' }) => ({
